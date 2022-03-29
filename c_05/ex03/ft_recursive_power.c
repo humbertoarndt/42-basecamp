f29d603d3b1a91e3a0d88d1393d629f090e27908
Humberto Arndt <humberto.arndt@gmail.com>
2022-03-29 14:27:25 -0300

    Add exercise

---
+++ c_05/ex03/ft_recursive_power.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: harndt <humberto.arndt@gmail.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/29 14:11:42 by harndt            #+#    #+#             */
+/*   Updated: 2022/03/29 14:26:49 by harndt           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int ft_recursive_power(int nb, int power)
+{
+	int	base;
+	
+	if (power == 0)
+		return (1);
+	else if (power < 0)
+		return (0);
+	else if (nb == 1)
+		return (nb);
+	else
+	{
+		return (nb * ft_recursive_power(nb, power -1));
+	}
+}

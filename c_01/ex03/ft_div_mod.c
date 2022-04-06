c2432417c0caa848f4bcd39eaa48760dd12ce524
Humberto Arndt <humberto.arndt@gmail.com>
2022-04-06 09:51:46 -0400

    Update

--- c_01/ex03/ft_div_mod.c
+++ c_01/ex03/ft_div_mod.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_div_mod.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: harndt <harndt@student.42sp.org.br>        +#+  +:+       +#+        */
+/*   By: harndt <humberto.arndt@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 20:00:15 by harndt            #+#    #+#             */
-/*   Updated: 2022/02/13 16:27:55 by harndt           ###   ########.fr       */
+/*   Updated: 2022/04/06 09:46:10 by harndt           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,4 @@ void	ft_div_mod(int a, int b, int *div, int *mod)
 {
 	*div = a / b;
 	*mod = a % b;
-}
-
-int	main(void)
-{
-	int a;
-	int b;
-	int div;
-	int mod;
-
-	a = 42;
-	b = 10;
-	div = 0;
-	mod = 0;
-	printf("A = %d\n", a);
-	printf("B = %d\n", b);
-	printf("DIV = %d\n", div);
-	printf("MOD = %d\n", mod);
-	printf("==DIV/MOD==\n");
-	ft_div_mod(a, b, &div, &mod);
-	printf("DIV = %d\n", div);
-	printf("MOD = %d\n", mod);
 }
